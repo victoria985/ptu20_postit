359db6ff5e49d37aa0ae7ac0908b5ca3df4c25b2
victoria985 <viktorijashot@gmail.com>
2024-03-05 12:01:04 +0200

    Update, Delete

--- postit_viktorija/postit_api/views.py
+++ postit_viktorija/postit_api/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics, permissions
+from rest_framework.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from . import models, serializers
 
 
@@ -9,4 +11,73 @@ class PostList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    gueryset =models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+    def delete(self, request, *args, **kwargs):
+        post = models.Post.objects.filter(
+            pk=kwargs['pk'], user=self.request.user
+        )
+        if post.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError(_("You can only delete your own content."))
+        
+
+    def put(self, request, *args, **kwargs):
+        post = models.Post.objects.filter(
+            pk=kwargs['pk'], user=self.request.user
+        )
+        if post.exists():
+            return self.update(request, *args, **kwargs)
+        else:
+            raise ValidationError(_("You can only edit your own content"))
+
+
+class CommentList(generics.ListCreateAPIView):
+    queryset = models.Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        post = models.Post.objects.get(pk=self.kwargs['pk'])
+        serializer.save(user=self.request.user, post=post)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        post = models.Post.objects.get(pk=self.kwargs['pk'])
+        return queryset.filter(post=post)   
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self, request, *args, **kwargs):
+        comment = models.Comment.objects.filter(
+            pk=kwargs['pk'], user=self.request.user
+        )
+        if comment.exists():
+            return self.destroy(request, *args, **kwargs)
+        else:
+            raise ValidationError(_("You can only delete your own content"))
+        
+    def put(self, request, *args, **kwargs):
+        comment = models.Comment.objects.filter(
+            pk=kwargs['pk'], user=self.request.user
+        )
+        if comment.exists():
+            return self.update(request, *args, **kwargs)
+        else:
+            raise ValidationError(_("You can only edit your content"))
+
+         
+            
+
 
